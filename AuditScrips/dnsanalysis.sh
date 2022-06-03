d873a862819c8358a26960c1b3e1e8cdf10fa78c
Fatake <fatake@fatake.com>
2022-06-03 15:31:35 -0500

    añadido de diccionarios

--- AuditScrips/dnsanalysis.sh
+++ AuditScrips/dnsanalysis.sh
@@ -46,27 +46,29 @@ done
 cat pentest/cewl_dict_*.txt |  iconv -f utf8 -t ascii//TRANSLIT  | awk -F ',' '{a[$1] += $2} END{for (i in a) print i, a[i]}' | sort -nr -t " " -k 2,2 >> pentest/cewl_words_${NAME}_num.txt
 head -n 1000 pentest/cewl_words_${NAME}_num.txt | cut -d "," -f 1 >> pentest/custom_dictionary_${NAME}.lst
 
+
 # <----------------------- amass, subfinder assetfinder ---------------------->
 for d in $(cat pentest/targets/domains.txt); do 
     subfinder -d $d >> pentest/foundsubdomains.txt
     assetfinder -subs-only $d >> pentest/foundsubdomains.txt
     amass enum -norecursive -noalts -d $d >> pentest/foundsubdomains.txt
+    
 done 
 
 
 # <----------------------- DNS Brute ----------------------->
-: ' DNS Brute Force
+
 echo -e "\n${blueColour}[*]${endColour} DNS brute force${endColour}"
 for d in $(cat pentest/targets/domains.txt); do
-    amass enum -brute -w /opt/rlyeh/dictionaries/subdomain.all.txt -d $d >> pentest/foundsubdomains.txt
-    amass enum -brute -w pentest/custom_dictionary_${NAME}.lst -d $d >> pentest/foundsubdomains.txt
+    amass enum -brute -w AuditScrips/WorldList/DNS_plussFinancial.dic-d $d >> pentest/foundsubdomains.txt
+    #amass enum -brute -w pentest/custom_dictionary_${NAME}.lst -d $d >> pentest/foundsubdomains.txt
 
     ## next line dont uncomment, it is only for really special clients 
     ## and/or when thoroughness is mandatory, 
     ## better to be used whit a lot of open resolvers
-    #amass enum -brute -w /opt/rlyeh/dictionaries/subdomain.plusFinancial.plusSpanish.7k.lst -d $d >> pentest/foundsubdomains.txt
+    #amass enum -brute -w /opt/rlyeh/dictionaries/DNS_allsubdomains.dic -d $d >> pentest/foundsubdomains.txt
 done 
-'
+
 
 cat pentest/foundsubdomains.txt | sort -u > pentest/subdomains.txt
 
--- AuditScrips/scope.sh
+++ AuditScrips/scope.sh
@@ -3,12 +3,12 @@
 
 
 echo -e "\n${greenColour}[*]${endColour} Starting DNS resolutions"
-for h in $(cat pentest/targets/domains.txt pentest/targets/subdomains.txt);
-do ( host $h); 
+for h in $(cat pentest/targets/domains.txt pentest/targets/subdomains.txt); do 
+    host $h
 done > pentest/resolved_hosts.txt
 
-for h in $(cat pentest/targets/ipaddresses.txt); 
-do ( host $h); 
+for h in $(cat pentest/targets/ipaddresses.txt);  do 
+    host $h
 done > pentest/resolved_ptrs.txt
 
 cat pentest/resolved_hosts.txt | grep "has address" | sed 's/ has address /\t/g' | sort -u | cut -f2 > pentest/hosts2ipv4addreses.txt

2e5c47647a4e2b52e6d4c01f8d4736ae39e9b213
Paulo Ruiz <38116322+Fatake@users.noreply.github.com>
2022-06-08 21:26:00 -0400

    Update tcpipscan.sh

--- AuditScrips/tcpipscan.sh
+++ AuditScrips/tcpipscan.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo -e "${greenColour}[*]${endColour} Starting ${purpleColour}nmap${endColour}"
-git clone --recursive https://github.com/cthulhu897/autonmap.git
-mr -rf autonmap/.git
+git clone --recursive https://github.com/Fatake/autonmap.git
 
 if [ ! -d "pentest/nmap" ]  then
     run_cmd "mkdir pentest/nmap/"
 fi
+
 sudo autonmap/autonmap.sh -o pentest/nmap/"${NAME}" -t "-iL pentest/hosts2ipv4addreses.txt"
-python3 AuditScrips/ParserNMAP/parse_nmap.py -i pentest/nmap/${NAME}_autonmap_servicescan.xml -o pentest/nmap/nmap_parsed
+python3 AuditScrips/ParserNMAP/parse_nmap.py -i pentest/nmap/autonmap_${NAME}_openports.xml -o pentest/nmap/nmap_parsed
+
+rm -rf autonmap/

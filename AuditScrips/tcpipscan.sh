a4cf681ceaedc428982e8a8bb6ab22ae8a51dc2c
kali <fata@fatake.com.org>
2022-12-19 16:19:59 -0600

    Cambios en tcp ip scan

--- AuditScrips/tcpipscan.sh
+++ AuditScrips/tcpipscan.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-PROJECT_PATH="$(pwd)"
 
 echo -e "\n${yellowColour}Starting ${purpleColour}auto nmap${endColour}"
 
-## Dowloadling autonmap tool
-if [ ! -d "${PROJECT_PATH}/autonmap" ]; then
-    COMMAND="git clone --recursive https://github.com/Fatake/autonmap.git"
-    echo -e "${blueColour}[*]${endColour}Dowloading ${purpleColour}${COMMAND}${endColour}"
-    run_cmd "${COMMAND} && chown -R 1000:1000 ${PROJECT_PATH}/autonmap/"
-fi
-
-sudo ./autonmap/autonmap.sh -o "${NAME}" -t "-iL ${PROJECT_PATH}/Pentest_${NAME}/targets/allipaddreses.txt"
+AUTONMAP_PATH="/opt/AuditTool/autonmap/autonmap.sh"
+sudo bash ${AUTONMAP_PATH} -o "${NAME}" -t "-iL $(pwd)/Pentest_${NAME}/targets/allipaddreses.txt"
 mv autonmap_* ${PROJECT_PATH}/Pentest_${NAME}
 python3 ${PROJECT_PATH}/AuditScrips/ParserNMAP/parse_nmap.py -i Pentest_${NAME}/autonmap_${NAME}/${NAME}_syn_scan.xml -o Pentest_${NAME}/autonmap_${NAME}/nmap_parsed
